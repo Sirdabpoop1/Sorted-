564fd855ff44d369d3215685eedb132c21cc4fac
Sirdabpoop1 <adrian.pun27@stgeorges.bc.ca>
2024-10-22 11:28:00 -0700

    FIXED GOOGLE SHEETS

--- delegate_sorter/sorter/views.py
+++ delegate_sorter/sorter/views.py
@@ -5,10 +5,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .services import get_all_rows
 
-
-def index(request):
-    return HttpResponse("Welcome to the delegate sorter")
-
 def info(request):
     info = get_all_rows("Test Sheets")
     return render(request, 'display_info.html', {'info': info})
