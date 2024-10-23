89ee18a3f8abc2c4671471e161d0402b067ff2c6
Sirdabpoop1 <adrian.pun27@stgeorges.bc.ca>
2024-10-23 15:31:05 -0700

    FIXED GSPREAD JUST CHANGE TITLE OF SHEET

--- delegate_sorter/sorter/views.py
+++ delegate_sorter/sorter/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
-
-from django.http import HttpResponse
+    
 from .services import get_all_rows
+from django.shortcuts import render
 
 def info(request):
-    info = get_all_rows("Test Sheets")
+    info = get_all_rows("Test sheet")
     return render(request, 'display_info.html', {'info': info})
