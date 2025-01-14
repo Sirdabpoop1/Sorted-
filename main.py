a93685753150b3834a2dc0ae9a9e35422cff32a9
Sirdabpoop1 <adrian.pun27@stgeorges.bc.ca>
2025-01-14 21:43:19 +0800

    fihsof

--- main.py
+++ main.py
@@ -7,23 +7,38 @@ client = gspread.authorize(creds)
 sheet_id = "1Gj9VocX5pCg-YleZVxZ7bohpQZfr-dxtEUts06wW3cE"
 sh = client.open_by_key(sheet_id)
 raw_worksheet = sh.worksheet("Raw")
-rows = raw_worksheet.get_all_values()
+form = raw_worksheet.get_all_values()
 
-for i, row in enumerate(rows, start = 1):
+for i, row in enumerate(form, start = 1):
     points = 0
-    grade = raw_worksheet.cell(i + 1, 2).value
-    conferences_attended = raw_worksheet.cell(i + 1, 3).value
-    if grade:
-        grade = int(grade)
+    grade = raw_worksheet.cell(i + 1, 2).value 
+
+    if grade: 
+        grade = int(grade) 
+    
         if grade > 7 and grade < 10:
-            points += 1
+                points += 1
         elif grade >= 10:
-            points += 2
-    if conferences_attended:
-        conferences_total = int(conferences_attended)
-        if conferences_total >= 3:
-            points += conferences_total * 2
-    if grade:
+            points += 3
+        
+        conferences_attended = raw_worksheet.cell(i + 1, 3).value
+
+        if conferences_attended: 
+            conferences_total = int(conferences_attended)
+            if conferences_total >= 3:
+                points += conferences_total * 2
+        
+        for awards in row(i + 1):
+            if awards == "Best Delegate":
+                points += 4
+            elif awards == "Outstanding Delegate":
+                points += 3
+            elif awards == "Honorable Mention":
+                points += 2
+            elif awards == "Best Position Paper":
+                points += 1
+                
+
         print(grade)
         print(conferences_total)
         print(points)
