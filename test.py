7657a47363f52016bfc13a4c82265082803cbc14
Sirdabpoop1 <adrian.pun27@stgeorges.bc.ca>
2025-03-18 15:28:06 -0700

    tada!

--- test.py
+++ test.py
@@ -1,4 +1,16 @@
-list = [1,2,3,4,5]
+import gspread
+from google.oauth2.service_account import Credentials
 
-for x in list:
-    print(x)
+scopes = ["https://www.googleapis.com/auth/spreadsheets"]
+creds = Credentials.from_service_account_file("credentials.json", scopes=scopes)
+client = gspread.authorize(creds)
+
+sheet_id = "1MPxFQCkgprBk3mNUAO-ZWVZaIfobZ3Wd4wzrBSZAYEw"\
+
+sh = client.open_by_key(sheet_id)
+
+raw_worksheet = sh.worksheet("SGS")
+
+value = raw_worksheet.cell(2, 1).value
+
+print(value)
